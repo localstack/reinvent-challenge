a294c5293888235edd6c06648d66cdbeb2d56073
Anca G <49899593+tinyg210@users.noreply.github.com>
2023-11-20 20:21:36 +0100

    Update init-resources.sh

--- init-resources.sh
+++ init-resources.sh
@@ -2,8 +2,6 @@
 
 echo "STARTING INIT RESOURCES"
 
-export FINAL_LAMBDA_ID=$(date +%s):$RANDOM | md5sum | cut -d ' ' -f 1
-
 # Create Lambda function
 awslocal lambda create-function \
   --function-name first-step \
